41da58a65c48d107666d9d0c59d61c108b9ea186
Than-Trang <83278820+Than-Trang@users.noreply.github.com>
2022-11-18 09:00:34 +0700

    .

--- src/GiftBoxAnimation.js
+++ src/GiftBoxAnimation.js
@@ -4,6 +4,7 @@ import "./styles.css";
 import box from "./images/box.png";
 import boxLid from "./images/box-lid.png";
 import kuku from "./images/jump-character.png";
+import kiki from "./images/LG-EcomElite-png-1.png";
 // import ConfettiGenerator from "./CanvasConfetti";
 import Confetti from "./confetti/Confetti";
 import Confetti2 from "./confetti/Confetti";
@@ -62,10 +63,13 @@ export default function GiftBoxAnimation() {
       <Confetti open={jump === "jump"} />
       <Confetti2 className="haha" open={jump === "jump"} />
       <Confetti3 className="hihi" open={jump === "jump"} />
+      <Confetti className="hehe" open={jump === "jump"} />
+      <Confetti className="haihai" open={jump === "jump"} />
+      <Confetti className="hh" open={jump === "jump"} />
 
       <div className="img-container">
         <p className={`kuku ${jump} kaka`}>Chào mừng ngày quốc tế nam giới</p>
-
+        <img className={`kiki ${jump}`} src={kiki} alt="kiki" />
         <img className={`kuku ${jump}`} src={kuku} alt="kuku" />
         <button className="box" onClick={() => animate()}>
           <img className="box_2" src={box} alt="box" />
